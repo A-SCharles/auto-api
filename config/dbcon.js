3fe291364f0710a2de7a23e6c46b593b8b4f7fc9
A-SCharles <a.charles.edu@gmail.com>
2022-09-07 08:37:16 +0200

    createpool

--- config/dbcon.js
+++ config/dbcon.js
@@ -1,12 +1,13 @@
 require("dotenv").config()
 const mysql = require("mysql")
 
-var con = mysql.createConnection({
+var con = mysql.createPool({
 host : process.env.DBHOST,
 database : process.env.DBNAME,
 password : process.env.DBPASSWORD,
 user : process.env.DBUSER,
-multipleStatements : true
+multipleStatements : true,
+connectionLimit : 10
 })
 
 module.exports = con 
